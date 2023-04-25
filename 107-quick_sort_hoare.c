768e51a974ee4a3246dffce3a7a7a415ded4e3f3
Alejio20 <humbleotamiri@gmail.com>
2023-04-25 01:40:23 -0700

    Added quick_sort_hoare

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,90 @@
+#include "sort.h"
+
+/**
+ * swapping - Swap two integers in an array.
+ * @x: The first integer to swap.
+ * @y: The second integer to swap.
+ */
+void swapping(int *x, int *y)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+/**
+ * hoare_partition - Order a subset of an array of integers
+ *                   according to the hoare partition scheme.
+ * @arr: The array of integers.
+ * @size: The size of the array.
+ * @left: The starting index of the subset to order.
+ * @right: The ending index of the subset to order.
+ *
+ * Return: The final partition index.
+ *
+ * Description: Uses the last element of the partition as the pivot.
+ * Prints the array after each swap of two elements.
+ */
+int hoare_partition(int *arr, size_t size, int left, int right)
+{
+	int pivot, above, below;
+
+	pivot = arr[right];
+	for (above = left - 1, below = right + 1; above < below;)
+	{
+		do {
+			above++;
+		} while (arr[above] < pivot);
+		do {
+			below--;
+		} while (arr[below] > pivot);
+
+		if (above < below)
+		{
+			swapping(arr + above, arr + below);
+			print_array(arr, size);
+		}
+	}
+
+	return (above);
+}
+
+/**
+ * hoare_sort - Implement the quicksort algorithm through recursion.
+ * @arr: An array of integers to sort.
+ * @size: The size of the array.
+ * @left: The starting index of the array partition to order.
+ * @right: The ending index of the array partition to order.
+ *
+ * Description: Uses the Hoare partition scheme.
+ */
+void hoare_sort(int *arr, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = hoare_partition(arr, size, left, right);
+		hoare_sort(arr, size, left, part - 1);
+		hoare_sort(arr, size, part, right);
+	}
+}
+
+/**
+ * quick_sort_hoare - Sort an array of integers in ascending
+ *                    order using the quicksort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Hoare partition scheme. Prints
+ * the array after each swap of two elements.
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	hoare_sort(array, size, 0, size - 1);
+}
